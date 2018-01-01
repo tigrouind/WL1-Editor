8c1a4125392d95deeb4d2d392bd36dbbbd8e9be4
Tigrou Ind <tigrou.ind@gmail.com>
2018-01-01 16:40:31 +0100

    Use table layout

--- WLEditor/MainForm.Designer.cs
+++ WLEditor/MainForm.Designer.cs
@@ -61,23 +61,31 @@ namespace WLEditor
 			this.aToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.bToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.objectPictureBox = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.panel3 = new System.Windows.Forms.Panel();
 			this.LevelPanel.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.levelPictureBox)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.tilesPictureBox)).BeginInit();
 			this.menuStrip1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.objectPictureBox)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.panel3.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// LevelPanel
 			// 
 			this.LevelPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-									| System.Windows.Forms.AnchorStyles.Left) 
-									| System.Windows.Forms.AnchorStyles.Right)));
+			| System.Windows.Forms.AnchorStyles.Left) 
+			| System.Windows.Forms.AnchorStyles.Right)));
 			this.LevelPanel.AutoScroll = true;
 			this.LevelPanel.Controls.Add(this.levelPictureBox);
-			this.LevelPanel.Location = new System.Drawing.Point(144, 54);
+			this.LevelPanel.Location = new System.Drawing.Point(143, 3);
 			this.LevelPanel.Name = "LevelPanel";
-			this.LevelPanel.Size = new System.Drawing.Size(752, 405);
+			this.LevelPanel.Size = new System.Drawing.Size(1063, 571);
 			this.LevelPanel.TabIndex = 1;
 			this.LevelPanel.Visible = false;
 			// 
@@ -87,14 +95,14 @@ namespace WLEditor
 			this.levelPictureBox.Name = "levelPictureBox";
 			this.levelPictureBox.Size = new System.Drawing.Size(4096, 512);
 			this.levelPictureBox.TabIndex = 2;
-			this.levelPictureBox.TabStop = false;			
+			this.levelPictureBox.TabStop = false;
 			this.levelPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.LevelPictureBoxPaint);
 			this.levelPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LevelPictureBoxMouseDown);
 			this.levelPictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LevelPictureBoxMouseMove);
 			// 
 			// tilesPictureBox
 			// 
-			this.tilesPictureBox.Location = new System.Drawing.Point(10, 54);
+			this.tilesPictureBox.Location = new System.Drawing.Point(3, 3);
 			this.tilesPictureBox.Name = "tilesPictureBox";
 			this.tilesPictureBox.Size = new System.Drawing.Size(128, 256);
 			this.tilesPictureBox.TabIndex = 3;
@@ -107,40 +115,41 @@ namespace WLEditor
 			// 
 			this.levelComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.levelComboBox.FormattingEnabled = true;
-			this.levelComboBox.Location = new System.Drawing.Point(12, 27);
+			this.levelComboBox.Location = new System.Drawing.Point(3, 3);
 			this.levelComboBox.Name = "levelComboBox";
-			this.levelComboBox.Size = new System.Drawing.Size(184, 21);
+			this.levelComboBox.Size = new System.Drawing.Size(267, 24);
 			this.levelComboBox.TabIndex = 4;
 			this.levelComboBox.Visible = false;
 			this.levelComboBox.SelectedIndexChanged += new System.EventHandler(this.LevelComboBoxSelectedIndexChanged);
 			// 
 			// menuStrip1
 			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
 			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-									this.fileToolStripMenuItem,
-									this.viewToolStripMenuItem});
+			this.fileToolStripMenuItem,
+			this.viewToolStripMenuItem});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(908, 24);
+			this.menuStrip1.Size = new System.Drawing.Size(1209, 36);
 			this.menuStrip1.TabIndex = 5;
 			this.menuStrip1.Text = "menuStrip1";
 			// 
 			// fileToolStripMenuItem
 			// 
 			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-									this.loadToolStripMenuItem,
-									this.saveToolStripMenuItem,
-									this.saveAsToolStripMenuItem,
-									this.exitToolStripMenuItem});
+			this.loadToolStripMenuItem,
+			this.saveToolStripMenuItem,
+			this.saveAsToolStripMenuItem,
+			this.exitToolStripMenuItem});
 			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 32);
 			this.fileToolStripMenuItem.Text = "&File";
 			// 
 			// loadToolStripMenuItem
 			// 
 			this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
 			this.loadToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-			this.loadToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+			this.loadToolStripMenuItem.Size = new System.Drawing.Size(276, 32);
 			this.loadToolStripMenuItem.Text = "Load rom...";
 			this.loadToolStripMenuItem.Click += new System.EventHandler(this.LoadToolStripMenuItemClick);
 			// 
@@ -149,7 +158,7 @@ namespace WLEditor
 			this.saveToolStripMenuItem.Enabled = false;
 			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
 			this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-			this.saveToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(276, 32);
 			this.saveToolStripMenuItem.Text = "Save";
 			this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItemClick);
 			// 
@@ -158,29 +167,29 @@ namespace WLEditor
 			this.saveAsToolStripMenuItem.Enabled = false;
 			this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
 			this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
-									| System.Windows.Forms.Keys.S)));
-			this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+			| System.Windows.Forms.Keys.S)));
+			this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(276, 32);
 			this.saveAsToolStripMenuItem.Text = "Save As...";
 			this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.SaveAsToolStripMenuItemClick);
 			// 
 			// exitToolStripMenuItem
 			// 
 			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-			this.exitToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(276, 32);
 			this.exitToolStripMenuItem.Text = "Exit";
 			this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItemClick);
 			// 
 			// viewToolStripMenuItem
 			// 
 			this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-									this.regionsToolStripMenuItem,
-									this.objectsToolStripMenuItem,
-									this.scrollRegionToolStripMenuItem,
-									this.collidersToolStripMenuItem,
-									this.paletteToolStripMenuItem,
-									this.switchToolStripMenuItem});
+			this.regionsToolStripMenuItem,
+			this.objectsToolStripMenuItem,
+			this.scrollRegionToolStripMenuItem,
+			this.collidersToolStripMenuItem,
+			this.paletteToolStripMenuItem,
+			this.switchToolStripMenuItem});
 			this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
-			this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.viewToolStripMenuItem.Size = new System.Drawing.Size(65, 32);
 			this.viewToolStripMenuItem.Text = "&View";
 			// 
 			// regionsToolStripMenuItem
@@ -189,7 +198,7 @@ namespace WLEditor
 			this.regionsToolStripMenuItem.CheckOnClick = true;
 			this.regionsToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
 			this.regionsToolStripMenuItem.Name = "regionsToolStripMenuItem";
-			this.regionsToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+			this.regionsToolStripMenuItem.Size = new System.Drawing.Size(242, 32);
 			this.regionsToolStripMenuItem.Text = "Sectors";
 			this.regionsToolStripMenuItem.Click += new System.EventHandler(this.RegionsToolStripMenuItemClick);
 			// 
@@ -199,7 +208,7 @@ namespace WLEditor
 			this.objectsToolStripMenuItem.CheckOnClick = true;
 			this.objectsToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
 			this.objectsToolStripMenuItem.Name = "objectsToolStripMenuItem";
-			this.objectsToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+			this.objectsToolStripMenuItem.Size = new System.Drawing.Size(242, 32);
 			this.objectsToolStripMenuItem.Text = "Objects";
 			this.objectsToolStripMenuItem.Click += new System.EventHandler(this.ObjectsToolStripMenuItemClick);
 			// 
@@ -209,7 +218,7 @@ namespace WLEditor
 			this.scrollRegionToolStripMenuItem.CheckOnClick = true;
 			this.scrollRegionToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
 			this.scrollRegionToolStripMenuItem.Name = "scrollRegionToolStripMenuItem";
-			this.scrollRegionToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+			this.scrollRegionToolStripMenuItem.Size = new System.Drawing.Size(242, 32);
 			this.scrollRegionToolStripMenuItem.Text = "Scroll boundaries";
 			this.scrollRegionToolStripMenuItem.Click += new System.EventHandler(this.ScrollRegionToolStripMenuItemClick);
 			// 
@@ -217,18 +226,18 @@ namespace WLEditor
 			// 
 			this.collidersToolStripMenuItem.CheckOnClick = true;
 			this.collidersToolStripMenuItem.Name = "collidersToolStripMenuItem";
-			this.collidersToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+			this.collidersToolStripMenuItem.Size = new System.Drawing.Size(242, 32);
 			this.collidersToolStripMenuItem.Text = "Colliders";
 			this.collidersToolStripMenuItem.Click += new System.EventHandler(this.CollidersToolStripMenuItemClick);
 			// 
 			// paletteToolStripMenuItem
 			// 
 			this.paletteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-									this.classicToolStripMenuItem,
-									this.blackWhiteToolStripMenuItem,
-									this.autumnToolStripMenuItem});
+			this.classicToolStripMenuItem,
+			this.blackWhiteToolStripMenuItem,
+			this.autumnToolStripMenuItem});
 			this.paletteToolStripMenuItem.Name = "paletteToolStripMenuItem";
-			this.paletteToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+			this.paletteToolStripMenuItem.Size = new System.Drawing.Size(242, 32);
 			this.paletteToolStripMenuItem.Text = "Palette";
 			// 
 			// classicToolStripMenuItem
@@ -236,32 +245,32 @@ namespace WLEditor
 			this.classicToolStripMenuItem.Checked = true;
 			this.classicToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
 			this.classicToolStripMenuItem.Name = "classicToolStripMenuItem";
-			this.classicToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+			this.classicToolStripMenuItem.Size = new System.Drawing.Size(208, 32);
 			this.classicToolStripMenuItem.Text = "Classic";
 			this.classicToolStripMenuItem.Click += new System.EventHandler(this.ClassicToolStripMenuItemClick);
 			// 
 			// blackWhiteToolStripMenuItem
 			// 
 			this.blackWhiteToolStripMenuItem.Name = "blackWhiteToolStripMenuItem";
-			this.blackWhiteToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+			this.blackWhiteToolStripMenuItem.Size = new System.Drawing.Size(208, 32);
 			this.blackWhiteToolStripMenuItem.Text = "Black && white";
 			this.blackWhiteToolStripMenuItem.Click += new System.EventHandler(this.BlackWhiteToolStripMenuItemClick);
 			// 
 			// autumnToolStripMenuItem
 			// 
 			this.autumnToolStripMenuItem.Name = "autumnToolStripMenuItem";
-			this.autumnToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+			this.autumnToolStripMenuItem.Size = new System.Drawing.Size(208, 32);
 			this.autumnToolStripMenuItem.Text = "Autumn";
 			this.autumnToolStripMenuItem.Click += new System.EventHandler(this.AutumnToolStripMenuItemClick);
 			// 
 			// switchToolStripMenuItem
 			// 
 			this.switchToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-									this.noneToolStripMenuItem,
-									this.aToolStripMenuItem,
-									this.bToolStripMenuItem});
+			this.noneToolStripMenuItem,
+			this.aToolStripMenuItem,
+			this.bToolStripMenuItem});
 			this.switchToolStripMenuItem.Name = "switchToolStripMenuItem";
-			this.switchToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+			this.switchToolStripMenuItem.Size = new System.Drawing.Size(242, 32);
 			this.switchToolStripMenuItem.Text = "Block switch";
 			// 
 			// noneToolStripMenuItem
@@ -270,7 +279,7 @@ namespace WLEditor
 			this.noneToolStripMenuItem.CheckOnClick = true;
 			this.noneToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
 			this.noneToolStripMenuItem.Name = "noneToolStripMenuItem";
-			this.noneToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+			this.noneToolStripMenuItem.Size = new System.Drawing.Size(138, 32);
 			this.noneToolStripMenuItem.Text = "None";
 			this.noneToolStripMenuItem.Click += new System.EventHandler(this.NoneToolStripMenuItemClick);
 			// 
@@ -278,7 +287,7 @@ namespace WLEditor
 			// 
 			this.aToolStripMenuItem.CheckOnClick = true;
 			this.aToolStripMenuItem.Name = "aToolStripMenuItem";
-			this.aToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+			this.aToolStripMenuItem.Size = new System.Drawing.Size(138, 32);
 			this.aToolStripMenuItem.Text = "A";
 			this.aToolStripMenuItem.Click += new System.EventHandler(this.AToolStripMenuItemClick);
 			// 
@@ -286,13 +295,13 @@ namespace WLEditor
 			// 
 			this.bToolStripMenuItem.CheckOnClick = true;
 			this.bToolStripMenuItem.Name = "bToolStripMenuItem";
-			this.bToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+			this.bToolStripMenuItem.Size = new System.Drawing.Size(138, 32);
 			this.bToolStripMenuItem.Text = "B";
 			this.bToolStripMenuItem.Click += new System.EventHandler(this.BToolStripMenuItemClick);
 			// 
 			// objectPictureBox
 			// 
-			this.objectPictureBox.Location = new System.Drawing.Point(10, 316);
+			this.objectPictureBox.Location = new System.Drawing.Point(0, 265);
 			this.objectPictureBox.Name = "objectPictureBox";
 			this.objectPictureBox.Size = new System.Drawing.Size(64, 64);
 			this.objectPictureBox.TabIndex = 6;
@@ -301,16 +310,58 @@ namespace WLEditor
 			this.objectPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.TilesPictureBoxPaint);
 			this.objectPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TilesPictureBoxMouseDown);
 			// 
+			// panel1
+			// 
+			this.panel1.AutoSize = true;
+			this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.panel1.Controls.Add(this.levelComboBox);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 36);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1209, 30);
+			this.panel1.TabIndex = 7;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.tableLayoutPanel1);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel2.Location = new System.Drawing.Point(0, 66);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(1209, 577);
+			this.panel2.TabIndex = 3;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 140F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.LevelPanel, 1, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(1209, 577);
+			this.tableLayoutPanel1.TabIndex = 3;
+			// 
+			// panel3
+			// 
+			this.panel3.AutoSize = true;
+			this.panel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.panel3.Controls.Add(this.tilesPictureBox);
+			this.panel3.Controls.Add(this.objectPictureBox);
+			this.panel3.Location = new System.Drawing.Point(3, 3);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(134, 332);
+			this.panel3.TabIndex = 7;
+			// 
 			// MainForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(908, 471);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-			this.Controls.Add(this.objectPictureBox);
-			this.Controls.Add(this.levelComboBox);
-			this.Controls.Add(this.tilesPictureBox);
-			this.Controls.Add(this.LevelPanel);
+			this.ClientSize = new System.Drawing.Size(1209, 643);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.menuStrip1);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MainMenuStrip = this.menuStrip1;
@@ -323,8 +374,14 @@ namespace WLEditor
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.objectPictureBox)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.panel3.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
+
 		}
 		private System.Windows.Forms.PictureBox objectPictureBox;
 		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
@@ -350,6 +407,10 @@ namespace WLEditor
 		private System.Windows.Forms.ToolStripMenuItem classicToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem blackWhiteToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem autumnToolStripMenuItem;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Panel panel3;
 		
 
 			
