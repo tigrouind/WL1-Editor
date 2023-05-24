311fa072ddf00995b6881aff7f74170580451972
Tigrou Ind <tigrou.ind@gmail.com>
2023-05-24 11:13:25 +0200

    Switch to framework 4.8

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild.exe /p:Configuration=Release ".\WLEditor.sln" /t:Rebuild
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:Configuration=Release ".\WLEditor.sln" /t:Rebuild
 if %ERRORLEVEL% NEQ 0 pause
 
 "%PROGRAMFILES%\7-Zip\7z" a -tzip "WLEditor.zip" ^
