0907dc778984da2ecb8e114c3690d80f3afde0f3
Tigrou Ind <tigrou.ind@gmail.com>
2025-07-25 22:25:54 +0200

    Allow to edit paths using mouse

--- WLEditor/Overworld/OverworldForm.cs
+++ WLEditor/Overworld/OverworldForm.cs
@@ -65,6 +65,7 @@ namespace WLEditor
 			};
 			pathForm.PathChanged += (s, e) => SetChanges();
 			pathForm.GetAnimationIndex = () => animationIndex;
+			pictureBox1.MouseWheel += PictureBox1MouseWheel;
 
 			selection1.InvalidateSelection += (s, e) => pictureBox1.Invalidate(e.ClipRectangle);
 			selection2.InvalidateSelection += (s, e) => pictureBox2.Invalidate(e.ClipRectangle);
@@ -727,6 +728,11 @@ namespace WLEditor
 			int tilePosY = e.Location.Y / 8 / zoom;
 			int tilePos = tilePosX + tilePosY * 32;
 
+			if (pathMode)
+			{
+				pathForm.MouseEvent(e, status);
+			}
+
 			if ((e.Button == MouseButtons.Left || e.Button == MouseButtons.Right) && !pathMode)
 			{
 				if (tilePos != currentTile)
@@ -789,6 +795,14 @@ namespace WLEditor
 					pictureBox1.Invalidate(new Rectangle(x * 8 * zoom, y * 8 * zoom, 8 * zoom, 8 * zoom));
 					SetChanges();
 				}
+			}
+		}
+
+		void PictureBox1MouseWheel(object sender, MouseEventArgs e)
+		{
+			if (pictureBox1.ClientRectangle.Contains(e.Location))
+			{
+				PictureBox1MouseEvent(e, TileEventStatus.MouseWheel);
 			}
 		}
 
