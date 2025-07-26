f7adecfb44b04fee4ae56aa6bf9e9e622536f6be
Tigrou Ind <tigrou.ind@gmail.com>
2025-07-27 00:34:18 +0200

    Implement undo/redo for paths

---
+++ WLEditor/Overworld/Cloner.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+
+namespace WLEditor
+{
+	public class Cloner //do not allow cycles
+	{
+		public static T Clone<T>(T source)
+		{
+			var sourceType = source.GetType();
+			if (sourceType.IsArray)
+			{
+				return (T)CloneArray();
+			}
+
+			return (T)CloneObject();
+
+			object CloneArray()
+			{
+				var type = sourceType.GetElementType();
+				var array = source as Array;
+				var clonedArray = Array.CreateInstance(type, array.Length);
+
+				for (int i = 0; i < array.Length; i++)
+				{
+					var value = array.GetValue(i);
+					if (value != null)
+					{
+						clonedArray.SetValue(CloneField(value, type), i);
+					}
+				}
+
+				return clonedArray;
+			}
+
+			object CloneObject()
+			{
+				var target = Activator.CreateInstance(sourceType);
+				foreach (var item in sourceType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+				{
+					var value = item.GetValue(source);
+					if (value != null)
+					{
+						item.SetValue(target, CloneField(value, item.FieldType));
+					}
+				}
+
+				return target;
+			}
+
+			object CloneField(object value, Type type)
+			{
+				if (type.IsValueType || type.IsEnum || type == typeof(string))
+				{
+					return value;
+				}
+				else
+				{
+					return Clone(value);
+				}
+			}
+		}
+	}
+}
